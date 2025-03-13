f3265215cbc3d7dd97eee0fd775974d88969d64d
Omar <shikzadayevomar@gmail.com>
2025-03-13 11:47:32 +0400

    1

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@
 #include <chrono>
 #include <mutex>
 const int numst=6;
-const int numtr=6;
+const int numtr=2;
 std::mutex mutexS[numst];
 void trainBaku(int trainID) {
     int station=0;
     int direction=1;
     while(true) {
         mutexS[station].lock();
-        std::cout <<"Поезд "<< trainID <<"на станции "<<station<< std::endl;
+        std::cout <<"Поезд "<< trainID <<" на станции "<<station<< std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout<<"Поезд "<<trainID<<"покидает "<<station<<std::endl;
+        std::cout<<"Поезд "<<trainID<<" покидает "<<station<<std::endl;
         mutexS[station].unlock();
-        station=+direction;
+        station+=direction;
         if(station==0||station==numst-1) {
             direction=-1;
         }
