ed4f5eae4420157f232dbd4bae5879d356b29d80
Omar <shikzadayevomar@gmail.com>
2025-03-13 11:37:30 +0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <mutex>
+const int numst=6;
+const int numtr=6;
+std::mutex mutexS[numst];
+void trainBaku(int trainID) {
+    int station=0;
+    int direction=1;
+    while(true) {
+        mutexS[station].lock();
+        std::cout <<"Поезд "<< trainID <<"на станции "<<station<< std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout<<"Поезд "<<trainID<<"покидает "<<station<<std::endl;
+        mutexS[station].unlock();
+        station=+direction;
+        if(station==0||station==numst-1) {
+            direction=-1;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    }
+
+}
+
+int main() {
+    std::vector<std::thread> trains;
+    for(int i=0;i<numtr;i++) {
+        trains.emplace_back(trainBaku,i);
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    }
+    for(auto& train : trains) {
+        train.join();
+    }
+
+
+}
