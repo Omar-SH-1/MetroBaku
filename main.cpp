d33d172474d400f8d1c1be008862157b4337d57c
Omar <shikzadayevomar@gmail.com>
2025-03-15 10:02:35 +0400

    Bakmil---İcheriseher

--- main.cpp
+++ main.cpp
@@ -3,36 +3,51 @@
 #include <thread>
 #include <chrono>
 #include <mutex>
+#include <fstream>
+
 const int numst = 6;
 const int numtr = 2;
 std::mutex mutexS[numst];
 std::string stationNames[numst] = {"Бакмил", "Нариман Нариманов", "Гянджлик", "28 мая", "Сахиль", "Ичеришехер"};
 
-void trainBaku(int trainID) {
+void trainBaku(int trainID, const std::string &filename) {
     int station = 0;
     int direction = 1;
-    while (true) {
+    std::ofstream logFile(filename);
+    if (!logFile.is_open()) {
+        std::cerr << "Не удалось открыть файл для записи: " << filename << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < 12; i++) {
         mutexS[station].lock();
 
-        std::cout << "Поезд " << trainID << " на станции " << stationNames[station] << std::endl;
+        logFile << "Поезд " << trainID << " на станции " << stationNames[station] << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Поезд " << trainID << " покидает " << stationNames[station] << std::endl;
+
+        logFile << "Поезд " << trainID << " покидает " << stationNames[station] << std::endl;
+
         mutexS[station].unlock();
         station += direction;
         if (station == 0 || station == numst - 1) {
-            direction = -1;
+            direction = -direction;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
+    logFile.close();
 }
 
 int main() {
     std::vector<std::thread> trains;
     for (int i = 0; i < numtr; i++) {
-        trains.emplace_back(trainBaku, i);
+        std::string filename = "train_" + std::to_string(i) + "_log.txt";
+        trains.emplace_back(trainBaku, i, filename);
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
+
     for (auto& train : trains) {
         train.join();
     }
+
+    return 0;
 }
