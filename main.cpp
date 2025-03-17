96088b77229332fb3335b1005d747a366f21451c
Omar <shikzadayevomar@gmail.com>
2025-03-17 10:37:53 +0400

    back to Bakmil

--- main.cpp
+++ main.cpp
@@ -21,19 +21,29 @@ void trainBaku(int trainID, const std::string &filename) {
 
     for (int i = 0; i < 12; i++) {
         mutexS[station].lock();
-
         logFile << "Поезд " << trainID << " на станции " << stationNames[station] << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
-
         logFile << "Поезд " << trainID << " покидает " << stationNames[station] << std::endl;
-
         mutexS[station].unlock();
+
         station += direction;
         if (station == 0 || station == numst - 1) {
             direction = -direction;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
+
+    // Возвращаем поезд в депо "Бакмил"
+    while (station != 0) {
+        mutexS[station].lock();
+        logFile << "Поезд " << trainID << " на станции " << stationNames[station] << " (возвращение в депо)" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        logFile << "Поезд " << trainID << " покидает " << stationNames[station] << " (возвращение в депо)" << std::endl;
+        mutexS[station].unlock();
+        station--;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+    logFile << "Поезд " << trainID << " прибыл в депо Бакмил" << std::endl;
     logFile.close();
 }
 
